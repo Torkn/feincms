07d4083ddcc533200c974e123edd8e0e6244a335
Torkn <torkn@datagrowth.com>
2011-04-30 19:47:35 +1000

    Improve compatibility with django-debug-toolbar

--- feincms/views/base.py
+++ feincms/views/base.py
@@ -97,5 +97,13 @@ class Handler(object):
 
         return response
 
+    @property
+    def __name__(self):
+        """
+        Dummy property to make this handler behave like a normal function.
+        This property is used by django-debug-toolbar
+        """
+        return self.__class__.__name__
+
 #: Default handler
 handler = Handler()
